c801489a19e5c09d6a3b10e3a578463b0bf5ddbe
RonnieDipple <Ronniedipple@gmail.com>
2019-11-06 16:26:29 -0600

    Initialization

---
+++ src/main/java/com/lambdaschool/demo/JavaOrdersAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+@SpringBootApplication
+public class JavaOrdersAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOrdersAssignmentApplication.class, args);
+    }
+
+}
